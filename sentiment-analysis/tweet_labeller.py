35deca6376990a95107eb5c34616c3b89e2dcf00
Slav Basharov <slavbacharov@gmail.com>
2020-01-27 02:09:17 -0800

    Labeller script, happiness ranking added

--- sentiment-analysis/tweet_labeller.py
+++ sentiment-analysis/tweet_labeller.py
@@ -8,6 +8,8 @@ import re, string, random
 import sys
 import os
 import pandas as pd
+import glob
+import numpy as np
 
 def remove_noise(tweet_tokens, stop_words = ()):
 
@@ -42,7 +44,6 @@ def get_tweets_for_model(cleaned_tokens_list):
         yield dict([token, True] for token in tweet_tokens)
 
 def train_and_save():
-
     positive_tweets = twitter_samples.strings('positive_tweets.json')
     negative_tweets = twitter_samples.strings('negative_tweets.json')
     text = twitter_samples.strings('tweets.20150430-223406.json')
@@ -91,28 +92,66 @@ def train_and_save():
 
     print(classifier.show_most_informative_features(10))
 
-def classify_tweets(filename):
-    tweets = pd.read_csv(filename)
+class SentimentClassifier():
 
+    def __init__(self, folder):
+        self.folder = folder
+        f = open('sentiment-analysis/sentiment_classifier.pickle', 'rb')
+        self.classifier = pickle.load(f)
+        self.universities_by_sentiment = pd.read_csv('university-rankings-data/world_rankings_with_twitter.csv')
+        self.sentiments = pd.DataFrame(columns=['total', 'positive', 'negative', 'university'])
+        self.result = None
+        print(self.universities_by_sentiment)
+        f.close()
+
+    def get_uni_name_from_filename(self, filename):
+        return filename.lower().replace('_', ' ').replace('.csv', '').replace("{0}/".format(self.folder), '').title()
+
+    def classify_single(self, tweet):
+        """
+        Process a single tweet and classify it
+        """
+        text = tweet.full_text
+        custom_tokens = remove_noise(word_tokenize(text))
+        sentiment = self.classifier.classify(dict([token, True] for token in custom_tokens))
+        return self.classifier.classify(dict([token, True] for token in custom_tokens))
+
+    def classify_tweets(self, uni_name, filename):
+        tweets = pd.read_csv(os.path.join(self.folder, filename))
+        count = len(tweets.index)
+        positive = 0
+        negative = 0
+        if (count > 0):
+            print("Classifying tweets for {0}".format(os.path.join(self.folder, filename)))
+
+            # Get sentiments for this university
+            sentiments = tweets.apply(self.classify_single, axis=1)
+            if (len(sentiments) > 0):
+                # Count how many positive and negative
+                positive = sentiments.where(lambda x: x.str.lower() == "positive").count()
+                negative = sentiments.where(lambda x: x.str.lower() == "negative").count()
+                print ("Total : {0} | Positive: {1} | Negative: {2}".format(count, positive, negative))
+
+        self.sentiments = self.sentiments.append(pd.DataFrame({'total': [count], 'positive': [positive], 'negative': [negative], 'university': [uni_name]}, columns=['total', 'positive', 'negative', 'university']), ignore_index=True)
+        
+    def classify_folder(self):
+        # First load the list of unis from the original csv
+        unis = pd.read_csv('university-rankings-data/world_rankings_with_twitter.csv')
+        
+        for index, uni in unis.iterrows():
+            self.classify_tweets(uni['institution'], "{0}.csv".format(uni['institution'].lower().replace(' ', '_')))
+        
+        self.result = pd.concat([self.universities_by_sentiment, self.sentiments], axis=1, sort=False)
+        
+        positive_fraction = self.result['positive'].divide(self.result['total'].where(self.result['total'] != 0, np.nan))
+        print("happiness ranking: {0}".format(positive_fraction))
+        self.result['happiness_ranking'] = positive_fraction
+        self.result.to_csv("sentiment-analysis/universities_by_sentiment")
 
 def process_files():
-    f = open('sentiment_classifier.pickle', 'rb')
-    classifier = pickle.load(f)
-    f.close()
-
     # Iterate over all the tweets and add a new column "Sentiment" using pd
-
-    for filename in os.listdir('tweets'):
-        if filename.endswith(".csv"): 
-            classify_tweets(filename)
-            continue
-        else:
-            continue
-    
-    custom_tweet = "Why does the university not support minorities...devastated" 
-    custom_tokens = remove_noise(word_tokenize(custom_tweet))
-    print(custom_tweet, classifier.classify(dict([token, True] for token in custom_tokens)))
-
+    classifier = SentimentClassifier("tweets-data")
+    classifier.classify_folder()
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
